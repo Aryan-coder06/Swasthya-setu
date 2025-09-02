2dacc6e5dc52cb5f2cf98dbcfc22bb2e8efd0652
Anshdeep Singh <anshdeep2306@gmail.com>
2025-09-02 17:23:14 +0530

    Wrote the functionality for patient signup in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,9 +6,25 @@ const signupPatient=async (req, res) => {
         const {email, password}=req.body;
         console.log(email, password);
         const result=await supabase.auth.signUp({email, password});
-        
+
+        if(result.user== null){
+            return res.json({message: "User already signed up!"});
+        }
+
         console.log(result.data);
-        res.json(result.data);
+        res.json({
+            message: "Signup Successful",
+            user: {
+                id: result.user.id,
+                email: result.user.email,
+                phone: result.user.phone
+            },
+            session: {
+                access_token: result.session.access_token,
+                expires_in: result.session.expires_in,
+                refresh_token: result.session.refresh_token
+            }
+        });
 
     } catch (error) {
         console.log(error);
