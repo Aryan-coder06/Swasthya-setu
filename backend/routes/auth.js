5065c9ffa3c90d0d7e6471c557104799cc4d0773
Anshdeep-Singh-9 <anshdeep2306@gmail.com>
2025-10-12 11:26:13 +0530

    Delete backend directory

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router= express.Router();
-
-import { signupPatient, signinPatient } from "../controllers/authController.js"
-
-router.post("/signup/patient", signupPatient);
-router.post("/signin/patient", signinPatient);
-
-export default router;
